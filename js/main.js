b6cbe56044f53281ed75016c0aa94784defa9981
EugeneShatalin <shatalin_ei@mail.ru>
2021-05-08 15:53:25 +0600

    Loop processing and validation of user input.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let numberOfFilm = prompt('How many movies have you watched so far?')
+let numberOfFilm = +prompt('How many movies have you watched so far?');
 
 const personalMovieDB = {
     count: numberOfFilm,
@@ -8,16 +8,22 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let oneLastMovis = prompt('One of the last movies you watched?');
+    let appraisal = prompt('How much would you rate it?');
+    if (oneLastMovis !== '' && appraisal !== '' && oneLastMovis !== null && appraisal !== null && oneLastMovis.length <= 50) {
+        personalMovieDB.movies[`${oneLastMovis}`] = appraisal;
+    } else {
+        i--
+    }
 }
 
-let oneLastMovis1 = prompt('One of the last movies you watched?')
-let appraisalOne = prompt('How much would you rate it?')
-
-personalMovieDB.movies[`${oneLastMovis1}`] = appraisalOne
-
-let oneLastMovis2 = prompt('One of the last movies you watched?')
-let appraisalTwo = prompt('How much would you rate it?')
-
-personalMovieDB.movies[`${oneLastMovis2}`] = appraisalTwo
-
-console.log(personalMovieDB.movies)
+if (personalMovieDB.count < 10) {
+    alert('Quite a few movies watched.')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('You are a classic spectator')
+} else {
+    alert('You are a movie fan')
+}
